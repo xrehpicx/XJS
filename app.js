0a26d8867a75d09880c965681143ab7935b83854
Raj <raj.fps2000@gmail.com>
2019-09-13 21:44:48 +0530

    v2

---
+++ app.js
@@ -0,0 +1,5 @@
+x('#box1').click((obj) => {
+    console.log(this)
+}).hover(() => {
+    console.log('hovered')
+});
---
+++ src/xjs.js
@@ -0,0 +1,109 @@
+
+class DS {
+    getScript(scriptProp) {
+        var script = document.createElement("script");
+        script.src = scriptProp['src'];
+        if (scriptProp['integrity'] !== undefined)
+            script.integrity = scriptProp['integrity'];
+        if (scriptProp['crossorigin'] !== undefined)
+            script.setAttribute('crossorigin', scriptProp['crossorigin']);
+        document.head.appendChild(script);
+    };
+    getScripts(scripts) {
+        scripts.forEach(element => {
+            getScript(element);
+        });
+    };
+    removeScript(scriptList) {
+        var allScriptTags = document.querySelectorAll('script');
+        scriptList.forEach(ele => {
+            allScriptTags.forEach(element => {
+                if (ele.src === element.src)
+                    element.parentNode.removeChild(element);
+            });
+        })
+    }
+}
+
+class xjs {
+    xjs = true;
+    constructor(selector) {
+        if (typeof (selector) === 'string') {
+            this.domnod = document.querySelector(selector);
+        }
+        else if (typeof (selector) === 'object') {
+            this.domnod = selector;
+        }
+    }
+    get() {
+        return this.domnod;
+    }
+    inside(d) {
+        this.domnod.innerHTML = d ? d : '';
+        return this;
+    }
+    branch(e, re = 'c') {
+        let element = document.createElement(e);
+        this.domnod.appendChild(element);
+        let exjs = $(element, false);
+        if (re[0] === 'c') {
+            return exjs;
+        } else if (re[0] === 'p') {
+            return this;
+        }
+    }
+    fullScreen() {
+        if (this.domnod.requestFullscreen)
+            this.domnod.requestFullscreen();
+        else if (this.domnod.mozRequestFullScreen)
+            this.domnod.mozRequestFullScreen();
+        else if (this.domnod.webkitRequestFullscreen)
+            this.domnod.webkitRequestFullscreen();
+        else if (this.domnod.msRequestFullscreen)
+            this.domnod.msRequestFullscreen();
+        return this;
+    }
+    listen(...events) {
+        let eventtype = typeof (events[0]);
+        if (eventtype === 'string') {
+            for (var i = 0; i < events.length; i += 2) {
+                this.domnod.addEventListener(events[i], () => events[i + 1](this));
+            }
+        } else if (eventtype === 'object') {
+            events.forEach(event => {
+                this.domnod.addEventListener(event.e, () => event.f(this));
+            });
+        }
+        return this;
+    }
+    click(fun) {
+        this.domnod.addEventListener('click', () => fun(this));
+        return this;
+    }
+    hover(fun) {
+        this.domnod.addEventListener('mouseover', () => fun(this));
+        return this;
+    }
+
+}
+function locate(element) {
+    let parsedElement;
+    if (element.xjs)
+        parsedElement = element.get();
+    else
+        parsedElement = element;
+    let rect = parsedElement.getBoundingClientRect();
+    let simpleObj = {
+        x: rect.left,
+        y: rect.top,
+        w: rect.width,
+        h: rect.height,
+    }
+    return simpleObj;
+}
+function x(selector, orphan = true) {
+    let xjsobj = new xjs(selector);
+    return xjsobj;
+}
+
+
